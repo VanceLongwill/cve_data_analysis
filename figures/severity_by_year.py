e9170c801e460b1ee2b5882d1d9e75b0a5002065
Vance Longwill <vance@evren.co.uk>
2022-08-15 13:15:27 +0100

    Improve bar chart visualisation

--- figures/severity_by_year.py
+++ figures/severity_by_year.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
 import numpy as np
 
+severities = ['CRITICAL', 'HIGH', 'MEDIUM', 'LOW', 'None']
+
 def relative_freqs(x):
-    severities = ['CRITICAL', 'HIGH',  'LOW',  'MEDIUM', 'None']
     values = [x[sv] for sv in severities]
     s = np.nansum(values)
     return [v/s for v in values]
@@ -12,23 +14,24 @@ def relative_freqs(x):
 
 def severity_by_year(df: pd.DataFrame):
     grouped = df.groupby(['Year', 'SEVERITY'], dropna=False).sum()
-    print(grouped)
-
-    print(grouped.head(20))
-    # print(grouped.head(20).value_counts(normalize=True))
-
     pivoted = grouped.pivot_table('index', ['Year'], 'SEVERITY')
-
-    print(pivoted.head(20))
-
-    # pivoted = pivoted.replace(np.NaN, 0)
-    #pivoted['Relative freq MEDIUM'] = pivoted[severities].sum(axis=1)
-
+    pivoted = pivoted.loc[:, severities]
     pivoted = pivoted.fillna(0)
     pivoted = pivoted.apply(relative_freqs, axis=1, result_type='broadcast')
-    print(pivoted.head(20))
+    last_10_years = pivoted.tail(10)
 
-    #lp = sns.countplot(data=pivoted)
-    #lp.set_title("Severity by year")
-    b = pivoted.plot.bar()
+    bg = last_10_years.plot(kind='bar', color={
+        'CRITICAL': '#780000',
+        'HIGH': '#dc0000',
+        'MEDIUM': '#fd8c00',
+        'LOW': '#fdc500',
+        'None': 'grey',
+    })
+
+    # format decimal as percentage
+    bg.yaxis.set_major_formatter(mtick.PercentFormatter(1.0))
+    plt.title(
+        'Relative frequencies of the severity levels of vulnerabilities by year')
+    plt.ylabel('Relative frequency (percentage)')
+    plt.xlabel('Year')
     plt.show()
